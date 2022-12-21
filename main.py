2302fbae76a8fcb48f361a1e9a1f526440c497ab
Nikolay0909 <90627851+Nikolay0909@users.noreply.github.com>
2022-12-21 23:23:28 +0300

    1

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-from clases import Game
+import pygame
+import random
 
-while True:
-    try:
-        user = int(input('Угадайте число от 1 до 10! Ваш вариант:'))
-        g = Game(user)
-        if g[0]:
-            print("Вы угадали!")
-        else:
-            print("Вы не угадали! Число: " + g[1])
-
-        user = input('Вы хотите продолжить? (0 - выход)')
-        if user == '0':
-            break
-    except:
-        pass
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+# Задаем цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+# Создаем игру и окно
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+# Цикл игры
+running = True
+while running:
+    # Держим цикл на правильной скорости
+    clock.tick(FPS)
+    # Ввод процесса (события)
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            running = False
