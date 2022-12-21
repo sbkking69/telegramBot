4b81ebe3e3be22dcfc9f50409d37af0fea5f0952
Nikolay0909 <90627851+Nikolay0909@users.noreply.github.com>
2022-12-21 22:56:38 +0300

    first

--- clases.py
+++ clases.py
@@ -2,6 +2,6 @@ from random import randint
 def Game(hui):
     x = randint(1,10)
     if hui == x:
-        return True
+        return True, x
     if hui != x:
-        return False
+        return False, x
