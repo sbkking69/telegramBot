94294f6794f18bdec1c9abbc1dc712f8cf508438
Stepan <sbkking69@gmail.com>
2022-12-21 13:21:41 +0300

    Add file: database.txt and modeficate main.py: download BD

--- command.py
+++ command.py
@@ -1,6 +1,15 @@
+from classes import * 
+
+def plugTypeUser(val) -> TypeUser:
+    for item in TypeUser:
+        if val == item.value[0]:
+            return item
+
 def DownloadDataBase(path, dataList):
     with open(path, 'r+') as file:
         for line in file:
             line = line.split('~')
             if line[0] == '#':
-                dataList.append({'id'})
+                dataList.append(WorkShop(id=line[0], title=line[1], authour=line[2], data=line[3], prise=line[4]))
+            elif line[0] != '#':
+                dataList.append(User(id=line[0], name=line[1], type=line[2]))
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import telebot
-from data import *
+import data
 import command
+from classes import * 
 
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot(data.token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    command.DownloadDataBase(dataList=data.BaseDateList,path=data.pathBaseData)
+
     bot.send_message(message.chat.id, "Hi!")
 
 @bot.message_handler(commands=['help'])
