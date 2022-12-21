d33b22d72d3491e53a425a155e71bc9f573f5c69
Nikolay0909 <90627851+Nikolay0909@users.noreply.github.com>
2022-12-21 23:47:37 +0300

    123

--- clases.py
+++ clases.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+from random import randint
 class GameObject:
     def __init__(self):
         self.x = 0
@@ -24,9 +26,13 @@ class GameObject:
 class Boll(GameObject):
     def Start(self):
         super().Start()
+        self.y = -30
+        self.x = randint(31, 329)
 
     def Update(self):
         super().Update()
-        
+
     def Draw(self, screen, RED):
         pygame.draw.circle(screen, RED, (self.x, self.y), 30)
+
+
