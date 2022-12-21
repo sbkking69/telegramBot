72dbe1d9d6289133601227d60e524910ec37f3ab
Stepan <sbkking69@gmail.com>
2022-12-21 03:17:52 +0300

    Start creat telegramm-bot

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-a = input("Enter name:")
-print('Your name:' + a)
+import telebot
 
-print('Hi!')
+bot = telebot.TeleBot("5919401327:AAHlByvIwXn9EcE9oyWDJhyM_YfVBHB0DQQ")
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Hi!")
+
+bot.polling(none_stop=True)
