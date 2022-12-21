3a5382009c95e2b2f3a0bab9c954189d26b7b790
Stepan <sbkking69@gmail.com>
2022-12-21 22:53:15 +0300

    Game v1.0

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from clases import Game
 
+while True:
+    try:
+        user = int(input('Угадайте число от 1 до 10! Ваш вариант:'))
+        if Game(user):
+            print("Вы угадали!")
+        else:
+            print("Вы не угадали!")
+
+        user = input('Вы хотите продолжить? (0 - выход)')
+        if user == '0':
+            break
+    except:
+        pass
